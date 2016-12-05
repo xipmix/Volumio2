179e49044be7fa279bd07d03e7a014915816bb59
Xipmix <xipmix@users.noreply.github.com>
2016-12-05 23:54:29 +1100

    Output KERNEL_REV, instead of FIRMWARE_REV

--- app/plugins/system_controller/volumio_command_line_client/commands/kernelsource.sh
+++ app/plugins/system_controller/volumio_command_line_client/commands/kernelsource.sh
@@ -28,7 +28,7 @@ FIRMWARE_REV=`cat /boot/.firmware_revision`
 echo "Firmware revision is"  $FIRMWARE_REV 
 
 KERNEL_REV=`curl -L https://github.com/Hexxeh/rpi-firmware/raw/${FIRMWARE_REV}/git_hash`
-echo "Kernel revision is "$FIRMWARE_REV
+echo "Kernel revision is "$KERNEL_REV
 
 if [ "$ARCH" = armv7l ]; then
  echo "Getting modules symvers for V7 kernel"
