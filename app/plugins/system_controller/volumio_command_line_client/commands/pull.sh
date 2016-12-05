66e7acc248710aa55c05d9fdbe533ea6489a975a
Xipmix <xipmix@users.noreply.github.com>
2016-12-06 07:53:57 +1100

    pull.sh: Avoid the need to run as root
    Making the backup of /volumio in /volumio-current requires root.
    But nothing else uses this directory so we are free to use a different location.

--- app/plugins/system_controller/volumio_command_line_client/commands/pull.sh
+++ app/plugins/system_controller/volumio_command_line_client/commands/pull.sh
@@ -2,13 +2,15 @@
 
 cd /home/volumio
 echo "Backing Up current Volumio folder in /volumio-current"
-mv /volumio /volumio-current
+backup=/var/tmp/volumio-current
+[ -d "$backup" ] && rm -rf "$backup"
+mv /volumio "$backup"
 echo "Cloning Volumio Backend repo"
 git clone https://github.com/volumio/Volumio2.git /volumio
 echo "Copying Modules"
-cp -rp /volumio-current/node_modules /volumio/node_modules
+cp -rp "$backup"/node_modules /volumio/node_modules
 echo "Copying UI"
-cp -rp /volumio-current/http/www /volumio/http/www
+cp -rp "$backup"/volumio-current/http/www /volumio/http/www
 echo "Getting Network Manager"
 wget https://raw.githubusercontent.com/volumio/Build/master/volumio/bin/wireless.js -O /volumio/app/plugins/system_controller/network/wireless.js
 echo "Setting Proper permissions"
