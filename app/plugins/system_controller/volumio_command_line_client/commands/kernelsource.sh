07db068f1cc646e669a929b662f1cd19533c73f9
Xipmix <xipmix@users.noreply.github.com>
2016-12-06 00:08:12 +1100

    Indent the functions so they are more visible

--- app/plugins/system_controller/volumio_command_line_client/commands/kernelsource.sh
+++ app/plugins/system_controller/volumio_command_line_client/commands/kernelsource.sh
@@ -13,67 +13,67 @@ fi
 
 function getsources {
 
-FIRMWARE_REV=`cat /boot/.firmware_revision`
-echo "Firmware revision is"  $FIRMWARE_REV 
+ FIRMWARE_REV=`cat /boot/.firmware_revision`
+ echo "Firmware revision is"  $FIRMWARE_REV
 
-KERNEL_REV=`curl -L https://github.com/Hexxeh/rpi-firmware/raw/${FIRMWARE_REV}/git_hash`
-echo "Kernel revision is "$KERNEL_REV
+ KERNEL_REV=`curl -L https://github.com/Hexxeh/rpi-firmware/raw/${FIRMWARE_REV}/git_hash`
+ echo "Kernel revision is "$KERNEL_REV
 
-if [ "$ARCH" = armv7l ]; then
- echo "Getting modules symvers for V7 kernel"
- curl -L https://github.com/Hexxeh/rpi-firmware/raw/${FIRMWARE_REV}/Module7.symvers >Module7.symvers
- else
- echo "Getting modules symvers for V6 kernel"
- curl -L https://github.com/Hexxeh/rpi-firmware/raw/${FIRMWARE_REV}/Module.symvers >Module.symvers
-fi
+ if [ "$ARCH" = armv7l ]; then
+  echo "Getting modules symvers for V7 kernel"
+  curl -L https://github.com/Hexxeh/rpi-firmware/raw/${FIRMWARE_REV}/Module7.symvers >Module7.symvers
+  else
+  echo "Getting modules symvers for V6 kernel"
+  curl -L https://github.com/Hexxeh/rpi-firmware/raw/${FIRMWARE_REV}/Module.symvers >Module.symvers
+ fi
 
-echo "Donwloading Kernel source tarball from " https://github.com/raspberrypi/linux/archive/${KERNEL_REV}.tar.gz
-curl -L https://github.com/raspberrypi/linux/archive/${KERNEL_REV}.tar.gz >rpi-linux.tar.gz
+ echo "Donwloading Kernel source tarball from " https://github.com/raspberrypi/linux/archive/${KERNEL_REV}.tar.gz
+ curl -L https://github.com/raspberrypi/linux/archive/${KERNEL_REV}.tar.gz >rpi-linux.tar.gz
 
 }
 export -f getsources
 
 function kernelinstall {
 
-echo " ---- VOLUMIO RASPBERRY PI KERNEL SOURCE DOWNLOADER ----"
-echo " "
-echo "This process might take a long time"
-echo " "
+ echo " ---- VOLUMIO RASPBERRY PI KERNEL SOURCE DOWNLOADER ----"
+ echo " "
+ echo "This process might take a long time"
+ echo " "
 
-ARCH=`/usr/bin/arch`
+ ARCH=`/usr/bin/arch`
 
-echo "Checking if build essential is installed"
-if [ $(dpkg-query -W -f='${Status}' make 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "Installing build essential"
-  apt-get update && apt-get install -y build-essential bc;
-fi
+ echo "Checking if build essential is installed"
+ if [ $(dpkg-query -W -f='${Status}' make 2>/dev/null | grep -c "ok installed") -eq 0 ];
+ then
+   echo "Installing build essential"
+   apt-get update && apt-get install -y build-essential bc;
+ fi
 
-cd /home/volumio
-su volumio -c "bash -c getsources"
+ cd /home/volumio
+ su volumio -c "bash -c getsources"
 
-echo "creating /usr/src/rpi-linux folder"
-mkdir /usr/src/rpi-linux
+ echo "creating /usr/src/rpi-linux folder"
+ mkdir /usr/src/rpi-linux
 
-echo "Extracting Kernel"
-tar --strip-components 1 -xf rpi-linux.tar.gz -C /usr/src/rpi-linux
-cd /usr/src/rpi-linux
+ echo "Extracting Kernel"
+ tar --strip-components 1 -xf rpi-linux.tar.gz -C /usr/src/rpi-linux
+ cd /usr/src/rpi-linux
 
-echo "Cloning current config"
-/sbin/modprobe configs
-gunzip -c /proc/config.gz >.config
+ echo "Cloning current config"
+ /sbin/modprobe configs
+ gunzip -c /proc/config.gz >.config
 
-echo "Copying modules symverse"
-if [ "$ARCH" = armv7l ]; then
- cp /home/volumio/Module7.symvers Module.symvers
-else
- cp /home/volumio/Module.symvers Module.symvers
-fi
-make modules_prepare
-echo "Linking Modules"
-ln -sv /usr/src/rpi-linux /lib/modules/$(uname -r)/build
-echo " "
-echo "Done, you can now build and install out of kernel modules"
+ echo "Copying modules symverse"
+ if [ "$ARCH" = armv7l ]; then
+  cp /home/volumio/Module7.symvers Module.symvers
+ else
+  cp /home/volumio/Module.symvers Module.symvers
+ fi
+ make modules_prepare
+ echo "Linking Modules"
+ ln -sv /usr/src/rpi-linux /lib/modules/$(uname -r)/build
+ echo " "
+ echo "Done, you can now build and install out of kernel modules"
 }
 
 kernelinstall
